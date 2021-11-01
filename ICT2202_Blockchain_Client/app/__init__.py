340ecd0438f92eefcbea464543aa4fed085674fb
WongWeiXuan <wong_weixuan@hotmail.com>
2021-11-02 00:42:59 +0800

    Update __init__.py

--- ICT2202_Blockchain_Client/app/__init__.py
+++ ICT2202_Blockchain_Client/app/__init__.py
@@ -25,6 +25,21 @@ bg_scheduler = BackgroundScheduler()
 bg_scheduler.add_job(func=sync_schedule, trigger="interval", seconds=10)
 bg_scheduler.start()
 
+from app.models import Peers
+
+
+# Init the Peers Table db with values (hardcode)
+@event.listens_for(Peers.__table__, 'after_create')
+def insert_initial_values(*args, **kwargs):
+    init_list = [
+        ("127.0.0.1", 5000, "server"),
+        ("192.168.1.1", 5000, "client"),
+    ]
+    for (ip_address, port, server_type) in init_list:
+        db.session.add(Peers(ip_address=ip_address, port=port, server_type=server_type))
+    db.session.commit()
+
+
 # Build the database
 db.create_all()
 
