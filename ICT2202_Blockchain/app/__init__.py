20edbb6cc5ca066e1827ac9b93db5e3ff9cc4a54
WongWeiXuan <wong_weixuan@hotmail.com>
2021-11-05 12:48:17 +0800

    Generating exe

--- ICT2202_Blockchain/app/__init__.py
+++ ICT2202_Blockchain/app/__init__.py
@@ -1,6 +1,9 @@
 import atexit
+import os
+import sys
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers import interval
 from flask import Flask
 from flask_httpauth import HTTPTokenAuth
 from flask_sqlalchemy import SQLAlchemy
@@ -11,7 +14,14 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 
 # Configurations
-app.config.from_object('config')
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.dirname(sys.executable)
+elif __file__:
+    BASE_DIR = os.path.dirname(__file__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["THREADS_PER_PAGE"] = 2
+# app.config.from_object('config')
 
 auth = HTTPTokenAuth(scheme='Bearer')
 db = SQLAlchemy(app)
@@ -24,9 +34,10 @@ Session = scoped_session(session_factory)
 from app.controller import sync_schedule, send_unverified_block, check_twothird
 
 bg_scheduler = BackgroundScheduler()
-bg_scheduler.add_job(func=sync_schedule, trigger="interval", seconds=10)
-bg_scheduler.add_job(func=send_unverified_block, trigger="interval", seconds=10)
-bg_scheduler.add_job(func=check_twothird, trigger="interval", seconds=10)
+trigger = interval.IntervalTrigger(seconds=10)
+bg_scheduler.add_job(func=sync_schedule, trigger=trigger)
+bg_scheduler.add_job(func=send_unverified_block, trigger=trigger)
+bg_scheduler.add_job(func=check_twothird, trigger=trigger)
 bg_scheduler.start()
 
 from app import views
@@ -36,10 +47,14 @@ from app.models import Peers
 # Init the Peers Table db with values (hardcode)
 @event.listens_for(Peers.__table__, 'after_create')
 def insert_initial_values(*args, **kwargs):
-    init_list = [
-        ("127.0.0.1", 5000, "server"),
-        ("192.168.1.1", 5000, "client"),
-    ]
+    init_list = []
+    with open(os.path.join(BASE_DIR, 'nodes.txt'), "r") as f:
+        lines = f.readlines()
+
+        for line in lines:
+            (ip, port, servertype) = line.split(sep=',')
+            init_list.append((ip, int(port), servertype))
+
     for (ip_address, port, server_type) in init_list:
         db.session.add(Peers(ip_address=ip_address, port=port, server_type=server_type))
     db.session.commit()
